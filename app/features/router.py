ade95c5c2ba187e2ecdf5a139ab41e41473f6c74
EYOB <eyobzawude76@gmail.com>
2026-08-30 09:35:07 +0300

    Remove unused  module

--- app/features/router.py
+++ app/features/router.py
@@ -3,7 +3,7 @@ from app.features.auth import router as auth_router
 from app.features.students import router as students_router
 from app.features.teachers import router as teachers_router
 from app.features.departments import router as departments_router
-from app.features.marks import router as marks_routergi
+from app.features.marks import router as marks_router
 from app.features.reports import router as reports_router
 from app.features.academic_records import router as academic_records_router
 from app.features.committee import router as committee_router
